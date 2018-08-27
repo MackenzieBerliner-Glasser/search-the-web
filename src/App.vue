ec7654f8bdf6a34fd79cfbfe7de6e1abb2597577
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-08-27 15:09:53 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/books">Books</router-link>
+      </nav>
+    </header>
+
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  components: {
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/router.js
@@ -0,0 +1,11 @@
+import VueRouter from 'vue-router';
+import Home from './components/Home.vue';
+import Books from './components/Books.vue';
+
+export default new VueRouter({
+  routes: [
+    { path: '/', component: Home },
+    { path: '/books', component: Books },
+    { path: '*', redirect: '/' }
+  ]
+});
